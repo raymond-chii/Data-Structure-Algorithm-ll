212d16d257c014e0c3b6e18f50261102eb7679b1
raymond-chii <raymondchi56@gmail.com>
2024-12-10 20:52:59 -0500

    correct

--- Dynamic_Programming/dyprog2.cpp
+++ Dynamic_Programming/dyprog2.cpp
@@ -3,10 +3,10 @@
 #include <string>
 #include <algorithm>
 
-bool Matrix[1001][1001];
+bool dp[1001][1001];
 
 std::string mergeStrings(std::string line1, std::string line2, std::string line3) {
-    memset(Matrix, false, sizeof(Matrix));
+    memset(dp, false, sizeof(dp));
 
     int len1 = line1.length();
     int len2 = line2.length();
@@ -16,44 +16,43 @@ std::string mergeStrings(std::string line1, std::string line2, std::string line3
         return "*** NOT A MERGE ***";
     }
 
-    Matrix[0][0] = true;
+    dp[0][0] = true;
 
     // Initialize first row
     for (int j = 1; j <= len2; j++) {
-        Matrix[0][j] = Matrix[0][j - 1] && (line2[j - 1] == line3[j - 1]);
+        dp[0][j] = dp[0][j - 1] && (line2[j - 1] == line3[j - 1]);
     }
 
     // Initialize first column
     for (int i = 1; i <= len1; i++) {
-        Matrix[i][0] = Matrix[i - 1][0] && (line1[i - 1] == line3[i - 1]);
+        dp[i][0] = dp[i - 1][0] && (line1[i - 1] == line3[i - 1]);
     }
 
     for (int i = 1; i <= len1; i++) {
         for (int j = 1; j <= len2; j++) {
             char c = line3[i + j - 1];
             if (line1[i - 1] == c && line2[j - 1] != c) {
-                Matrix[i][j] = Matrix[i - 1][j];
+                dp[i][j] = dp[i - 1][j];
             }
             else if (line1[i - 1] != c && line2[j - 1] == c) {
-                Matrix[i][j] = Matrix[i][j - 1];
+                dp[i][j] = dp[i][j - 1];
             }
             else if (line1[i - 1] == c && line2[j - 1] == c) {
-                Matrix[i][j] = Matrix[i - 1][j] || Matrix[i][j - 1];
+                dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
             }
         }
     }
 
-    if (!Matrix[len1][len2]) {
+    if (!dp[len1][len2]) {
         return "*** NOT A MERGE ***";
     }
 
-    // Modified reconstruction to ensure leftmost placement of uppercase letters
     std::string result = line3;
     int j = len2;
     
     for (int i = len1; i > 0; i--) {
         while (true) {
-            if (Matrix[i][j] && (j == 0 || !Matrix[i][j - 1])) {
+            if (dp[i][j] && (j == 0 || !dp[i][j - 1])) {
                 result[i + j - 1] -= 32;  // Convert to uppercase
                 break;
             }
